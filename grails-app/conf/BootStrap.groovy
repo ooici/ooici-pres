2543e863878634277b3246cf5073a0a74c73dd98
Stephen Pasco <stephen.pasco@gmail.com>
2011-03-08 20:24:10 -0800

    commented out conflicting boostrap code that doesn't work with grails 1.3.7

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -10,25 +10,25 @@ class BootStrap {
     def init = { servletContext ->
 
 	    // Setting up Users and their roles
-	    def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
-		def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
-
-		String adminPassword = springSecurityService.encodePassword('admin')
-	    String testPassword = springSecurityService.encodePassword('test')
-
-		def adminUser = new User(username: 'admin', enabled: true, password: adminPassword)
-		def testUser = new User(username: 'test', enabled: true, password: testPassword)
-
-	    adminUser.save(flush: true)
-		testUser.save(flush: true)
-
-	    UserRole.create adminUser, adminRole, true
-		UserRole.create testUser, userRole, true
-
-		assert User.count() == 2
-		assert Role.count() == 2
-
-		assert UserRole.count() == 2
+//	    def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+//		def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+//
+//		String adminPassword = springSecurityService.encodePassword('admin')
+//	    String testPassword = springSecurityService.encodePassword('test')
+//
+//		def adminUser = new User(username: 'admin', enabled: true, password: adminPassword)
+//		def testUser = new User(username: 'test', enabled: true, password: testPassword)
+//
+//	    adminUser.save(flush: true)
+//		testUser.save(flush: true)
+//
+//	    UserRole.create adminUser, adminRole, true
+//		UserRole.create testUser, userRole, true
+//
+//		assert User.count() == 2
+//		assert Role.count() == 2
+//
+//		assert UserRole.count() == 2
 
 	    // Initiating ION Messaging Base Process
 	    BootstrapIONService.bootstrap()
