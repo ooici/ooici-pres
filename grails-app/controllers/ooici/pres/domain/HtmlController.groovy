a9b7b36b220252ffa1c094aa9efc8b2a6998d25c
Stephen Pasco <stephen.pasco@gmail.com>
2011-03-13 17:25:05 -0700

    added render template code for fetchDataResourceDetails originating from jQuery

--- grails-app/controllers/ooici/pres/domain/HtmlController.groovy
+++ grails-app/controllers/ooici/pres/domain/HtmlController.groovy
@@ -1,13 +1,29 @@
 package ooici.pres.domain
 
 /**
- * This class serves HTML to jQuery load() calls
+ * This class serves HTML to jQuery Ajax load() calls
+ *
+ * @author Stephen Pasco
  */
 class HtmlController {
 
     def index = { }
 
+	/**
+	 * Fetches the data resource and returns HTML using a template
+	 */
 	def fetchDataResourceDetails =  {
+
+		/**
+		 * TODO: Implement this piece once we have round-trip Grails to App Service layer calls working
+		 */
+
+//		def dr = DataResource.get(params.id)
+		// Gets the _dataresourcedetail template and passes it the dataresource object
+//		render(template:"dataresourcedetail", model:[dataresource:dr])
+
 		
+		render(template:"dataresourcedetail", model:[dataResourceTitle:params.title])
+
 	}
 }
