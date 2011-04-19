ed69f0323c303557bd7f5f0f8b3208e5ff5011cc
Thomas Lennan <thomaslennan@gmail.com>
2011-04-19 16:06:36 -0700

    refactor of controllers

--- grails-app/services/ooici/pres/BootstrapIONService.groovy
+++ grails-app/services/ooici/pres/BootstrapIONService.groovy
@@ -1,5 +1,9 @@
 package ooici.pres
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import ion.core.BaseProcess
 import ion.resource.ServiceRDO
 import ion.core.data.DataObjectManager
@@ -37,6 +41,44 @@ class BootstrapIONService  {
 	
 	def destroy() {
 		appIntegrationService.dispose()
+	}
+
+	
+	def String sendReceive(AppIntegrationService.RequestType requestType, String requestString, String ooi_id, String expiry, int status) {
+
+		String responseString = BootstrapIONService.appIntegrationService.sendReceiveUIRequest(requestString, requestType, ooi_id, expiry);
+
+		status = BootstrapIONService.appIntegrationService.getStatus();
+		if (status != 200) {
+			def errorMessage = BootstrapIONService.appIntegrationService.getErrorMessage();
+			System.out.println("Error:\nrequest type: " + requestType + "\nrequest message: " + requestString + "\nooi_id: " + ooi_id + "\nexpiry: " + expiry + "\nStatus: " + status + "\nError message: " + errorMessage);
+			responseString = "{\"Error\": \"" + errorMessage  + "\"}";
+		}
+
+		return responseString;
+	}
+
+	
+	def getOoiIdAndExpiry(HttpServletRequest servletRequest, String ooi_id, String expiry) {
+		HttpSession httpSession = servletRequest.getSession(true);
+		
+		ooi_id = httpSession.getAttribute("IONCOREOOIID") == null ? "ANONYMOUS" : httpSession.getAttribute("IONCOREOOIID");
+		expiry = httpSession.getAttribute("IONCOREEXPIRY") == null ? "ANONYMOUS" : httpSession.getAttribute("IONCOREEXPIRY");
+
+//		ooi_id = "ANONYMOUS";
+//		expiry = "0";
+//
+//		Cookie[] cookies = servletRequest.getCookies();
+//		if (cookies != null) {
+//			for (int i = 0; i < cookies.length; i++) {
+//				if (cookies[i].getName().equals("IONCOREOOIID")) {
+//					ooi_id = cookies[i].getValue();
+//				}
+//				if (cookies[i].getName().equals("IONCOREEXPIRY")) {
+//					expiry = cookies[i].getValue();
+//				}
+//			}
+//		}
 	}
 
 }
