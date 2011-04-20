fadc8b029dd7996563c1b716d2acf8a5642f600b
Thomas Lennan <thomaslennan@gmail.com>
2011-04-20 14:23:15 -0700

    Removed unnecessary common code as it was moved to base controller class

--- grails-app/services/ooici/pres/BootstrapIONService.groovy
+++ grails-app/services/ooici/pres/BootstrapIONService.groovy
@@ -43,42 +43,4 @@ class BootstrapIONService  {
 		appIntegrationService.dispose()
 	}
 
-	
-	def String sendReceive(AppIntegrationService.RequestType requestType, String requestString, String ooi_id, String expiry, int status) {
-
-		String responseString = BootstrapIONService.appIntegrationService.sendReceiveUIRequest(requestString, requestType, ooi_id, expiry);
-
-		status = BootstrapIONService.appIntegrationService.getStatus();
-		if (status != 200) {
-			def errorMessage = BootstrapIONService.appIntegrationService.getErrorMessage();
-			System.out.println("Error:\nrequest type: " + requestType + "\nrequest message: " + requestString + "\nooi_id: " + ooi_id + "\nexpiry: " + expiry + "\nStatus: " + status + "\nError message: " + errorMessage);
-			responseString = "{\"Error\": \"" + errorMessage  + "\"}";
-		}
-
-		return responseString;
-	}
-
-	
-	def getOoiIdAndExpiry(HttpServletRequest servletRequest, String ooi_id, String expiry) {
-		HttpSession httpSession = servletRequest.getSession(true);
-		
-		ooi_id = httpSession.getAttribute("IONCOREOOIID") == null ? "ANONYMOUS" : httpSession.getAttribute("IONCOREOOIID");
-		expiry = httpSession.getAttribute("IONCOREEXPIRY") == null ? "ANONYMOUS" : httpSession.getAttribute("IONCOREEXPIRY");
-
-//		ooi_id = "ANONYMOUS";
-//		expiry = "0";
-//
-//		Cookie[] cookies = servletRequest.getCookies();
-//		if (cookies != null) {
-//			for (int i = 0; i < cookies.length; i++) {
-//				if (cookies[i].getName().equals("IONCOREOOIID")) {
-//					ooi_id = cookies[i].getValue();
-//				}
-//				if (cookies[i].getName().equals("IONCOREEXPIRY")) {
-//					expiry = cookies[i].getValue();
-//				}
-//			}
-//		}
-	}
-
 }
