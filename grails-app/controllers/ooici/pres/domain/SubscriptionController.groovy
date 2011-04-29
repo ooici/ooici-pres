b561186ef2cef238fbefa7ec2d1945200ddac188
Thomas Lennan <thomaslennan@gmail.com>
2011-04-29 13:03:09 -0700

    added find action, included ooi id param on create action

--- grails-app/controllers/ooici/pres/domain/SubscriptionController.groovy
+++ grails-app/controllers/ooici/pres/domain/SubscriptionController.groovy
@@ -5,10 +5,21 @@ import ion.integration.ais.AppIntegrationService;
 import ion.integration.ais.AppIntegrationService.RequestType;
 
 class SubscriptionController extends BaseController {
+	
+	def find = {
+		
+		preProcessRequest(true);
+		
+		params.put("user_ooi_id", ooi_id);
+
+		sendReceive(RequestType.FIND_DATA_RESOURCE_SUBSCRIPTION)
+	}
 
 	def create = {
 		
 		preProcessRequest(true);
+		
+		params.put("user_ooi_id", ooi_id);
 
 		sendReceive(RequestType.CREATE_DATA_RESOURCE_SUBSCRIPTION)
 	}
