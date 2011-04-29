747443ccde986175b1b8d1ba816845312310e925
Thomas Lennan <thomaslennan@gmail.com>
2011-04-29 13:17:59 -0700

    added logging (printfs) for debug purposes, disabled invalidation of certificate based on not before time

--- src/java/cilogon/SuccessServlet.java
+++ src/java/cilogon/SuccessServlet.java
@@ -5,6 +5,7 @@ import ion.integration.ais.AppIntegrationService.RequestType;
 
 import java.io.PrintWriter;
 import java.net.URI;
+import java.security.Principal;
 import java.security.cert.X509Certificate;
 import java.util.Map;
 
@@ -55,151 +56,175 @@ public class SuccessServlet extends PortalAbstractServlet {
 	final String MARINE_OPERATOR_ROLE = "MARINE_OPERATOR";
 
 	protected void doIt(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Throwable {
-    	
-        httpServletResponse.setContentType("text/html");
-        PrintWriter pw = httpServletResponse.getWriter();
 
-        String identifier = clearCookie(httpServletRequest, httpServletResponse);
-        if (identifier == null) {
-            throw new ServletException("Error: No identifier for this delegation request was found. ");
-        }
-        CILogonService cis = new CILogonService(getPortalEnvironment());
-        PortalCredentials credential = cis.getCredential(identifier);
-        
-        X509Certificate certificate = credential.getX509Certificate();
-        long startDateMS = certificate.getNotBefore().getTime();
-        long expirationDateMS = certificate.getNotAfter().getTime();
-        long currentDateMS = System.currentTimeMillis();
+		httpServletResponse.setContentType("text/html");
+		PrintWriter pw = httpServletResponse.getWriter();
 
-        // Ensure certificate is currently valid
-        if (startDateMS < currentDateMS && currentDateMS < expirationDateMS) {
-        	// Authenticate user with ION
-        	// Unfortunately, the strings returned from the util need a bit of massaging
-        	// to make them valid Json to pass to ION
-        	String certificateString = getSecurityUtil().toPEM(certificate);
-        	certificateString = certificateString.substring(0, certificateString.lastIndexOf("\n"));
-        	certificateString = certificateString.replace("\r", "");
-        	certificateString = certificateString.replace("\n", "\\n");
-        	
-        	String privateKeyString = getSecurityUtil().toPEM(credential.getPrivateKey());
-        	privateKeyString = privateKeyString.replace("\r", "");
-        	privateKeyString = privateKeyString.replace("\n", "\\n");
-        	
-    		String request = "{\"certificate\": \"" + certificateString + "\",";
-    		request += "\"rsa_private_key\": \"" + privateKeyString + "\"}";
-    		
-        	String result = BootstrapIONService.appIntegrationService.sendReceiveUIRequest(request, RequestType.REGISTER_USER, "ANONYMOUS", "0");
+		String identifier = clearCookie(httpServletRequest, httpServletResponse);
+		if (identifier == null) {
+			throw new ServletException("Error: No identifier for this delegation request was found. ");
+		}
+		CILogonService cis = new CILogonService(getPortalEnvironment());
+		PortalCredentials credential = cis.getCredential(identifier);
 
-        	if (result != null && result.length() > 0) {
-        		HttpSession session = httpServletRequest.getSession(true);
+		X509Certificate certificate = credential.getX509Certificate();
 
-    			String ooi_id = "";
-    			boolean userIsAdmin = false;
-    			boolean userIsEarlyAdopter = false;
-    			boolean userIsDataProvider = false;
-    			boolean userIsMarineOperator = false;
-    			boolean userIsAlreadyRegistered = false;
+		long startDateMS = certificate.getNotBefore().getTime();
+		long expirationDateMS = certificate.getNotAfter().getTime();
+		long currentDateMS = System.currentTimeMillis();
 
-        		try {
-        			// Extract values from Json result
-        			JSONObject valueMap = (JSONObject)JSON.parse(result);
+		// For debugging purposes
+		Principal principal = certificate.getSubjectDN();
+		String subjectDN = principal.getName();
 
-        			ooi_id = (String)valueMap.get(USER_ID_KEY);
-        			userIsAdmin = (Boolean)valueMap.get(USER_IS_ADMIN_KEY);
-        			userIsEarlyAdopter = (Boolean)valueMap.get(USER_IS_EARY_ADOPTER_KEY);
-        			userIsDataProvider = (Boolean)valueMap.get(USER_IS_DATA_PROVIDER_KEY);
-        			userIsMarineOperator = (Boolean)valueMap.get(USER_IS_MARINE_OPERATOR_KEY);
-        			userIsAlreadyRegistered = (Boolean)valueMap.get(USER_ALREADY_REGISTERED_KEY);
-        		}
-        		catch (Exception e) {
-        			System.out.println("Exception extracting ooi id or user roles from user registration response");
-        		}
+		principal = certificate.getIssuerDN();
+		String issuerDN = principal.getName();
 
-        		if (ooi_id.length() != 0) {
-        			int expiry = (int)((expirationDateMS - currentDateMS)/1000);
+		System.out.println("SuccessServlet: received authentication request for <" + subjectDN + ">");
 
-        			session.setAttribute(OOI_ID_KEY, ooi_id);
-        			session.setAttribute(EXPIRY_KEY, "" + expirationDateMS/1000);
-        		}
+		// Ensure certificate is currently valid
+		// TODO worry about the start date?
+		if (currentDateMS < expirationDateMS) {
+			// Authenticate user with ION
+			// Unfortunately, the strings returned from the util need a bit of massaging
+			// to make them valid Json to pass to ION
+			String certificateString = getSecurityUtil().toPEM(certificate);
+			certificateString = certificateString.substring(0, certificateString.lastIndexOf("\n"));
+			certificateString = certificateString.replace("\r", "");
+			certificateString = certificateString.replace("\n", "\\n");
 
-        		String authorityRole = "ROLE_USER";
-        		if (userIsAdmin) {
-        			authorityRole = "ROLE_ADMIN";
-            		session.setAttribute(USER_IS_ADMIN_KEY, true);
-        		}
-        		else {
-            		session.setAttribute(USER_IS_ADMIN_KEY, false);
-        		}
+			String privateKeyString = getSecurityUtil().toPEM(credential.getPrivateKey());
+			privateKeyString = privateKeyString.replace("\r", "");
+			privateKeyString = privateKeyString.replace("\n", "\\n");
 
-        		if (userIsEarlyAdopter) {
-            		session.setAttribute(USER_IS_EARY_ADOPTER_KEY, true);
-        		}
-        		else {
-            		session.setAttribute(USER_IS_EARY_ADOPTER_KEY, false);
-        		}
+			String request = "{\"certificate\": \"" + certificateString + "\",";
+			request += "\"rsa_private_key\": \"" + privateKeyString + "\"}";
 
-        		if (userIsDataProvider) {
-            		session.setAttribute(DATA_PROVIDER_ROLE, true);
-        		}
-        		else {
-            		session.setAttribute(DATA_PROVIDER_ROLE, false);
-        		}
+			String result = BootstrapIONService.appIntegrationService.sendReceiveUIRequest(request, RequestType.REGISTER_USER, "ANONYMOUS", "0");
 
-        		if (userIsMarineOperator) {
-            		session.setAttribute(MARINE_OPERATOR_ROLE, true);
-        		}
-        		else {
-            		session.setAttribute(MARINE_OPERATOR_ROLE, false);
-        		}
+			if (result != null && result.length() > 0) {
+				HttpSession session = httpServletRequest.getSession(true);
 
-        		if (userIsAlreadyRegistered) {
-            		session.setAttribute(USER_ALREADY_REGISTERED_KEY, true);
-        		}
-        		else {
-            		session.setAttribute(USER_ALREADY_REGISTERED_KEY, false);
-        		}
+				String ooi_id = "";
+				boolean userIsAdmin = false;
+				boolean userIsEarlyAdopter = false;
+				boolean userIsDataProvider = false;
+				boolean userIsMarineOperator = false;
+				boolean userIsAlreadyRegistered = false;
 
-        		PreAuthenticatedAuthenticationToken token = new PreAuthenticatedAuthenticationToken(
-        				ooi_id, new GrantedAuthority[] { new GrantedAuthorityImpl(authorityRole) });
-        		token.setAuthenticated(true);
-        		SecurityContextHolder.getContext().setAuthentication(token);
-        		
-    			Map map = ConfigurationHolder.getConfig().flatten();
-        		String mainUrl = (String)map.get("ioncore.mainurl");
-        			
-        		if (mainUrl == null) {
-        			mainUrl = "/main";
-        		}
-        			
-        		URI redirectUri = new URI(httpServletRequest.getContextPath() + mainUrl);
-        		httpServletResponse.sendRedirect(redirectUri.toString());
-        	}
-        	else {
-        		int status = BootstrapIONService.appIntegrationService.getStatus();
-        		String errorMessage = BootstrapIONService.appIntegrationService.getErrorMessage();
-        		
-    			System.out.println("Error received on findResources\nRequest message: " + request + "\nStatus: " + status + "\nError message: " + errorMessage);
+				try {
+					// Extract values from Json result
+					JSONObject valueMap = (JSONObject)JSON.parse(result);
 
-    			URI redirectUri = new URI(httpServletRequest.getContextPath());
-        		httpServletResponse.sendRedirect(redirectUri.toString());
-        	}
-        }
-        else {
-        	System.out.println("Received certificate has invalid time value");
+					ooi_id = (String)valueMap.get(USER_ID_KEY);
+					userIsAdmin = (Boolean)valueMap.get(USER_IS_ADMIN_KEY);
+					userIsEarlyAdopter = (Boolean)valueMap.get(USER_IS_EARY_ADOPTER_KEY);
+					userIsDataProvider = (Boolean)valueMap.get(USER_IS_DATA_PROVIDER_KEY);
+					userIsMarineOperator = (Boolean)valueMap.get(USER_IS_MARINE_OPERATOR_KEY);
+					userIsAlreadyRegistered = (Boolean)valueMap.get(USER_ALREADY_REGISTERED_KEY);
+				}
+				catch (Exception e) {
+					System.out.println("Exception extracting ooi id or user roles from user registration response");
+				}
 
-    		URI redirectUri = new URI(httpServletRequest.getContextPath());
-    		httpServletResponse.sendRedirect(redirectUri.toString());
-        }
-    }
+				if (ooi_id.length() != 0) {
+					int expiry = (int)((expirationDateMS - currentDateMS)/1000);
 
-  public SecurityUtil getSecurityUtil() {
-	  if (securityUtil == null) {
-		  securityUtil = new SecurityUtil();
-	  }
-	  return securityUtil;
-  }
+					session.setAttribute(OOI_ID_KEY, ooi_id);
+					session.setAttribute(EXPIRY_KEY, "" + expirationDateMS/1000);
+				}
+				System.out.println("SuccessServlet: OOI ID for <" + subjectDN + ">: " + ooi_id);
 
-  SecurityUtil securityUtil;
+				String authorityRole = "ROLE_USER";
+				if (userIsAdmin) {
+					authorityRole = "ROLE_ADMIN";
+					session.setAttribute(USER_IS_ADMIN_KEY, true);
+					System.out.println("SuccessServlet: <" + subjectDN + "> isAdmin: True");
+				}
+				else {
+					session.setAttribute(USER_IS_ADMIN_KEY, false);
+					System.out.println("SuccessServlet: <" + subjectDN + "> isAdmin: False");
+				}
+
+				if (userIsEarlyAdopter) {
+					session.setAttribute(USER_IS_EARY_ADOPTER_KEY, true);
+					System.out.println("SuccessServlet: <" + subjectDN + "> isEarlyAdopter: True");
+				}
+				else {
+					session.setAttribute(USER_IS_EARY_ADOPTER_KEY, false);
+					System.out.println("SuccessServlet: <" + subjectDN + "> isEarlyAdopter: False");
+				}
+
+				if (userIsDataProvider) {
+					session.setAttribute(DATA_PROVIDER_ROLE, true);
+					System.out.println("SuccessServlet: <" + subjectDN + "> isDataProvider: True");
+				}
+				else {
+					session.setAttribute(DATA_PROVIDER_ROLE, false);
+					System.out.println("SuccessServlet: <" + subjectDN + "> isDataProvider: False");
+				}
+
+				if (userIsMarineOperator) {
+					session.setAttribute(MARINE_OPERATOR_ROLE, true);
+					System.out.println("SuccessServlet: <" + subjectDN + "> isMarineOperator: True");
+				}
+				else {
+					session.setAttribute(MARINE_OPERATOR_ROLE, false);
+					System.out.println("SuccessServlet: <" + subjectDN + "> isMarineOperator: False");
+				}
+
+				if (userIsAlreadyRegistered) {
+					session.setAttribute(USER_ALREADY_REGISTERED_KEY, true);
+					System.out.println("SuccessServlet: <" + subjectDN + "> isAlreadyRegistered: True");
+				}
+				else {
+					session.setAttribute(USER_ALREADY_REGISTERED_KEY, false);
+					System.out.println("SuccessServlet: <" + subjectDN + "> isAlreadyRegistered: False");
+				}
+
+				PreAuthenticatedAuthenticationToken token = new PreAuthenticatedAuthenticationToken(
+						ooi_id, new GrantedAuthority[] { new GrantedAuthorityImpl(authorityRole) });
+				token.setAuthenticated(true);
+				SecurityContextHolder.getContext().setAuthentication(token);
+
+				Map map = ConfigurationHolder.getConfig().flatten();
+				String mainUrl = (String)map.get("ioncore.mainurl");
+
+				if (mainUrl == null) {
+					mainUrl = "/main";
+				}
+
+				URI redirectUri = new URI(httpServletRequest.getContextPath() + mainUrl);
+				httpServletResponse.sendRedirect(redirectUri.toString());
+			}
+			else {
+				int status = BootstrapIONService.appIntegrationService.getStatus();
+				String errorMessage = BootstrapIONService.appIntegrationService.getErrorMessage();
+
+				System.out.println("SuccessServlet: Error received logging in <" + subjectDN + ">\nRequest message: " + request + "\nStatus: " + status + "\nError message: " + errorMessage);
+
+				URI redirectUri = new URI(httpServletRequest.getContextPath());
+				httpServletResponse.sendRedirect(redirectUri.toString());
+			}
+		}
+		else {
+			System.out.println("SuccessServlet: Received certificate for <" + subjectDN + "> has invalid time value");
+			System.out.println("SuccessServlet: current time MS       <" + currentDateMS + ">");
+			System.out.println("SuccessServlet: certificate expiry MS <" + expirationDateMS + ">");
+
+			URI redirectUri = new URI(httpServletRequest.getContextPath());
+			httpServletResponse.sendRedirect(redirectUri.toString());
+		}
+	}
+
+	public SecurityUtil getSecurityUtil() {
+		if (securityUtil == null) {
+			securityUtil = new SecurityUtil();
+		}
+		return securityUtil;
+	}
+
+	SecurityUtil securityUtil;
 
 
 }
