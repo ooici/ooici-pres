7b73b40bed39cac87af569e421673b52c39dc8cf
Thomas Lennan <thomaslennan@gmail.com>
2011-05-16 17:11:49 -0700

    Updated ionproto version number

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -18,7 +18,7 @@ grails.project.dependency.resolution = {
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-		compile 'net.ooici:ionproto:0.3.21-pl4',
+		compile 'net.ooici:ionproto:0.3.22-pl1',
 		        'net.ooici:ioncore-java:0.2.6-dev'
     }
 }
