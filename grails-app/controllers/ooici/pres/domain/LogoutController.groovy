aa0f0db651262d6eaf97ffc0991eed11a0a443a8
Thomas Lennan <thomaslennan@gmail.com>
2011-05-19 14:33:36 -0700

    Changed logout controller to zap roles from session

--- grails-app/controllers/ooici/pres/domain/LogoutController.groovy
+++ grails-app/controllers/ooici/pres/domain/LogoutController.groovy
@@ -8,6 +8,11 @@ class LogoutController {
 	def index = {
 		session.removeAttribute("IONCOREOOID")
 		session.removeAttribute("IONCOREEXPIRY")
+		session.removeAttribute("user_is_admin")
+		session.removeAttribute("user_is_early_adopter")
+		session.removeAttribute("user_is_data_provider")
+		session.removeAttribute("user_is_marine_operator")
+		session.removeAttribute("user_already_registered")
 
 		redirect(uri:"/")
 	}
