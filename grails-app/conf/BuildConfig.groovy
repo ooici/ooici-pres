e05830d4dcdaae44038afa026a54c3a0d0886a26
Thomas Lennan <thomaslennan@gmail.com>
2011-05-20 14:45:26 -0700

    Updated dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -18,7 +18,7 @@ grails.project.dependency.resolution = {
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-        compile 'net.ooici:ionproto:0.3.24-pl7',
+        compile 'net.ooici:ionproto:0.3.24-pl8',
                 'net.ooici:ioncore-java:0.2.8-dev'
     }
 }
