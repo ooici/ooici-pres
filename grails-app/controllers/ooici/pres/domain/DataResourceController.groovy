1b501e400ca54da5725252a7d5c5b9a5929f029f
Thomas Lennan <thomaslennan@gmail.com>
2011-05-20 13:21:48 -0700

    Prettying up the code

--- grails-app/controllers/ooici/pres/domain/DataResourceController.groovy
+++ grails-app/controllers/ooici/pres/domain/DataResourceController.groovy
@@ -1,18 +1,18 @@
 package ooici.pres.domain
 
-import ion.integration.ais.AppIntegrationService.RequestType;
+import ion.integration.ais.AppIntegrationService.RequestType
 
 class DataResourceController extends BaseController {
 
 	def find = {
 		
-		preProcessRequest(false);
+		preProcessRequest(false)
 		
 		specialPreProcessFindRequest()
 		
 		params.put("user_ooi_id", ooi_id)
 
-		sendReceive(RequestType.FIND_DATA_RESOURCES);
+		sendReceive(RequestType.FIND_DATA_RESOURCES)
 	}
 	
 	def findByUser = {
@@ -73,43 +73,43 @@ class DataResourceController extends BaseController {
 		
 		// Special handle numeric params
 		if (params.get("minLongitude") != null)
-			params.put("minLongitude",params.float("minLongitude"));
+			params.put("minLongitude",params.float("minLongitude"))
 		if (params.get("maxLongitude") != null)
-			params.put("maxLongitude",params.float("maxLongitude"));
+			params.put("maxLongitude",params.float("maxLongitude"))
 		if (params.get("minLatitude") != null)
-			params.put("minLatitude",params.float("minLatitude"));
+			params.put("minLatitude",params.float("minLatitude"))
 		if (params.get("maxLatitude") != null)
-			params.put("maxLatitude",params.float("maxLatitude"));
+			params.put("maxLatitude",params.float("maxLatitude"))
 		if (params.get("minVertical") != null)
-			params.put("minVertical",params.float("minVertical"));
+			params.put("minVertical",params.float("minVertical"))
 		if (params.get("maxVertical") != null)
-			params.put("maxVertical",params.float("maxVertical"));
+			params.put("maxVertical",params.float("maxVertical"))
 	}
 	
 	def specialPreProcessBooleanAndTimeValues = {
 		
 		// Special handle numeric params
 		if (params.get("update_interval_seconds") != null)
-			params.put("update_interval_seconds",params.long("update_interval_seconds"));
+			params.put("update_interval_seconds",params.long("update_interval_seconds"))
 		if (params.get("max_ingest_millis") != null)
-			params.put("max_ingest_millis",params.long("max_ingest_millis"));
+			params.put("max_ingest_millis",params.long("max_ingest_millis"))
 		if (params.get("update_start_datetime_millis") != null)
-			params.put("update_start_datetime_millis",params.long("update_start_datetime_millis"));
+			params.put("update_start_datetime_millis",params.long("update_start_datetime_millis"))
 		if (params.get("is_public") != null)
-			params.put("is_public",params.boolean("is_public"));
+			params.put("is_public",params.boolean("is_public"))
 	}
 	
 	def specialPreProcessBounds = {
 		
 		// Special handle numeric params
 		if (params.get("request_bounds_north") != null)
-			params.put("request_bounds_north",params.float("request_bounds_north"));
+			params.put("request_bounds_north",params.float("request_bounds_north"))
 		if (params.get("request_bounds_south") != null)
-			params.put("request_bounds_south",params.float("request_bounds_south"));
+			params.put("request_bounds_south",params.float("request_bounds_south"))
 		if (params.get("request_bounds_west") != null)
-			params.put("request_bounds_west",params.float("request_bounds_west"));
+			params.put("request_bounds_west",params.float("request_bounds_west"))
 		if (params.get("request_bounds_east") != null)
-			params.put("request_bounds_east",params.float("request_bounds_east"));
+			params.put("request_bounds_east",params.float("request_bounds_east"))
 	}
 
 }
