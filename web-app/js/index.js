750b30e21df7e35b11f9b9804bd2f084b7ef9ead
Alex Clemesha <clemesha@ucsd.edu>
2011-08-16 18:51:21 -0700

    New hoverstate images for landing page buttons. Add all missing href links.

--- web-app/js/index.js
+++ web-app/js/index.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+  $(".access_button").hover(function(){$(this).css("background-position","0px -24px");}, function(){$(this).css("background-position","0px 0px");});
+  $(".access_button").mousedown(function(){$(this).css("background-position","0px -50px");});
+  $("#enterion-content").hover(function(){$(this).css("background-position","0px -103px");}, function(){$(this).css("background-position","0px 0px");});
+  $("#enterion-content").mousedown(function(){$(this).css("background-position","0px -204px");});
 	$("#registration_link").colorbox({
         inline:true, 
         href:"#registration_dialog", 
