15d15319fd4e28c5a5e7b107d405c09d1737f9fb
unwin <unwin@buildbot.oceanobservatories.org>
2011-08-22 14:08:06 -0700

    updated a test

--- selenium_testing/test/Test_0A_FULL_LOGOUT_ERROR_OK.java
+++ selenium_testing/test/Test_0A_FULL_LOGOUT_ERROR_OK.java
@@ -9,21 +9,26 @@ import java.util.regex.Pattern;
 public class Test_0A_FULL_LOGOUT_ERROR_OK extends SeleneseTestCase {
 	@Before
 	public void setUp() throws Exception {
-		selenium = new DefaultSelenium("sg-hub.oceanobservatories.org", 4444, "IE on Windows7", "https://buildbot.oceanobservatories.org:8080/");
+		selenium = new DefaultSelenium("sg-hub.oceanobservatories.org", 4444, "IE on Windows7", "https://buildbot.oceanobservatories.org:9443/");
 		selenium.start();
 	}
 
 	@Test
 	public void test_0A_FULL_LOGOUT_ERROR_OK() throws Exception {
-		selenium.open("https://buildbot.oceanobservatories.org:8080/ooici-pres-0.1/dashboard");
-		for (int second = 0;; second++) {
+		selenium.open("/ooici-pres-0.1/dashboard");
+                for (int second = 0;; second++) {
 			if (second >= 60) fail("timeout");
 			try { if (selenium.isTextPresent("Showing 1 to")) break; } catch (Exception e) {}
 			Thread.sleep(1000);
 		}
 
-		selenium.click("logout_link");
+		selenium.click("id=logout_link");
 		selenium.waitForPageToLoad("30000");
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("Welcome to Release 1 of the Integrated")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
 	}
 
 	@After
