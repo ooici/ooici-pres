82c6fd65db6ed042deaa9a344083befbaaa01eb4
Roger Unwin <unwin@sdsc.edu>
2011-08-24 13:08:01 -0700

    Added Notifications Test. It fails in local selenium, Maybe it will work in selenium grid. Manual walk throughs of this workflow succeed outside of selenium.

---
+++ selenium_testing/test/Test_13_Notification_setup.java
@@ -0,0 +1,80 @@
+package com.example.tests;
+
+import com.thoughtworks.selenium.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.regex.Pattern;
+
+public class Test_13_Notification_setup extends SeleneseTestCase {
+	@Before
+	public void setUp() throws Exception {
+		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "https://buildbot.oceanobservatories.org:9443/");
+		selenium.start();
+	}
+
+	@Test
+	public void test_13_Notification_setup() throws Exception {
+		selenium.open("/ooici-pres-0.1/index.html");
+		selenium.click("login_button");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("wayflogonbutton");
+		selenium.waitForPageToLoad("30000");
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("Cilogon.org is asking for some information from your Google Account. To see and approve the request, sign in.")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		selenium.type("Email", "U_S_E_R-N_A_M_E");
+		selenium.type("Passwd", "P_A_S_S-W_O_R_D");
+		selenium.click("signIn");
+		selenium.waitForPageToLoad("30000");
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("Showing 1 to 8 of 8 entries")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		selenium.click("//tr[@id='3319A67F-81F3-424F-8E69-4F28C4E04806']/td[1]");
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("Title: Moana Loa Data Source")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		selenium.click("setup_notifications");
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("Send notifications when:")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		selenium.click("updateWhenAvailable");
+		selenium.click("datasourceIsOffline");
+		Thread.sleep(1000);
+		selenium.click("start_notifications");
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if ("Notification saved".equals(selenium.getAlert())) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		assertEquals("Notification saved", selenium.getAlert());
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("Showing 1 to 1 of 1 entries")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		selenium.click("radioMySub");
+		selenium.click("//input[@type='checkbox']");
+		selenium.click("delete_selected");
+		selenium.click("radioMySub");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+}
