148b88d8809bf374e3abe7ff0dd64fb85041f392
Roger Unwin <unwin@sdsc.edu>
2011-08-30 11:19:36 -0700

    Re-recorded

--- selenium_testing/test/Test_10_View_Vertical_Extent.java
+++ selenium_testing/test/Test_10_View_Vertical_Extent.java
@@ -15,56 +15,9 @@ public class Test_10_View_Vertical_Extent extends SeleneseTestCase {
 
 	@Test
 	public void test_10_View_Vertical_Extent() throws Exception {
-                selenium.open("/ooici-pres-0.1/index.html");
-                selenium.click("id=login_button");
-                selenium.waitForPageToLoad("30000");
-                for (int second = 0;; second++) {
-                        if (second >= 60) fail("timeout");
-                        try { if (selenium.isTextPresent("Select An Identity Provider:")) break; } catch (Exception e) {}
-                        Thread.sleep(1000);
-                }
-
-                selenium.click("id=wayflogonbutton");
-                selenium.waitForPageToLoad("30000");
-                for (int second = 0;; second++) {
-                        if (second >= 60) fail("timeout");
-                        try { if (selenium.isTextPresent("Cilogon.org is asking for some information from your Google Account. To see and approve the request, sign in.")) break; } catch (Exception e) {}
-                        Thread.sleep(1000);
-                }
-
-                selenium.uncheck("id=PersistentCookie");
-                selenium.type("id=Email", "U_S_E_R-N_A_M_E_1");
-                selenium.type("id=Passwd", "P_A_S_S-W_O_R_D_1");
-                selenium.click("id=signIn");
-                selenium.waitForPageToLoad("30000");
-                for (int second = 0;; second++) {
-                        if (second >= 60) fail("timeout");
-                        try { if (selenium.isTextPresent("Showing 1 to 8 of 8 entries")) break; } catch (Exception e) {}
-                        Thread.sleep(1000);
-                }
-
-
-
-
-
-/*
 		selenium.open("/ooici-pres-0.1/");
 		selenium.click("guest_button");
 		selenium.waitForPageToLoad("30000");
-		for (int second = 0;; second++) {
-			if (second >= 60) fail("timeout");
-			try { if (selenium.isTextPresent("Showing 1 to 8 of 8 entries")) break; } catch (Exception e) {}
-			Thread.sleep(1000);
-		}
-*/
-		selenium.click("radioAllPubRes");
-		for (int second = 0;; second++) {
-			if (second >= 60) fail("timeout");
-			try { if (selenium.isTextPresent("Showing 1 to 8 of 8 entries")) break; } catch (Exception e) {}
-			Thread.sleep(1000);
-		}
-
-		selenium.click("radioAllPubRes");
 		for (int second = 0;; second++) {
 			if (second >= 60) fail("timeout");
 			try { if (selenium.isTextPresent("Showing 1 to 8 of 8 entries")) break; } catch (Exception e) {}
@@ -82,6 +35,7 @@ public class Test_10_View_Vertical_Extent extends SeleneseTestCase {
 		selenium.type("ge_altitude_ub", "1");
 		selenium.type("ge_altitude_lb", "30");
 		selenium.click("vertical_extent_above");
+		Thread.sleep(1000);
 		selenium.click("apply_filter_button");
 		selenium.click("radioAllPubRes");
 		for (int second = 0;; second++) {
