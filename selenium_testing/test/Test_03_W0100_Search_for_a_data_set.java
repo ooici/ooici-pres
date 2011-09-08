169ae91c9a06f4de407b684fc331ac61090170f4
Roger Unwin <unwin@sdsc.edu>
2011-09-08 14:44:26 -0700

    doubled the length of timeouts in Test_03_W0100_Search_for_a_data_set.java

--- selenium_testing/test/Test_03_W0100_Search_for_a_data_set.java
+++ selenium_testing/test/Test_03_W0100_Search_for_a_data_set.java
@@ -17,9 +17,9 @@ public class Test_03_W0100_Search_for_a_data_set extends SeleneseTestCase {
 	public void test_03_W0100_Search_for_a_data_set() throws Exception {
 		selenium.open("/ooici-pres-0.1/");
 		selenium.click("login_button");
-		selenium.waitForPageToLoad("30000");
+		selenium.waitForPageToLoad("60000");
 		selenium.click("wayflogonbutton");
-		selenium.waitForPageToLoad("30000");
+		selenium.waitForPageToLoad("60000");
 		for (int second = 0;; second++) {
 			if (second >= 60) fail("timeout");
 			try { if (selenium.isTextPresent("Cilogon.org is asking for some information from")) break; } catch (Exception e) {}
@@ -29,7 +29,7 @@ public class Test_03_W0100_Search_for_a_data_set extends SeleneseTestCase {
 		selenium.type("Email", "U_S_E_R-N_A_M_E");
 		selenium.type("Passwd", "P_A_S_S-W_O_R_D");
 		selenium.click("signIn");
-		selenium.waitForPageToLoad("30000");
+		selenium.waitForPageToLoad("60000");
 		for (int second = 0;; second++) {
 			if (second >= 60) fail("timeout");
 			try { if (selenium.isTextPresent("Showing 1 to 8 of 8 entries")) break; } catch (Exception e) {}
@@ -81,11 +81,11 @@ public class Test_03_W0100_Search_for_a_data_set extends SeleneseTestCase {
 		}
 
 		selenium.click("logout_link");
-		selenium.waitForPageToLoad("30000");
+		selenium.waitForPageToLoad("60000");
 		selenium.open("http://google.com");
 		selenium.click("gbgs4");
 		selenium.click("gb_71");
-		selenium.waitForPageToLoad("30000");
+		selenium.waitForPageToLoad("60000");
 	}
 
 	@After
